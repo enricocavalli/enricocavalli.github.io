cd8f61c7ba054049078e5cb99d2c8fc31d69b8e9
Enrico Cavalli <enrico.cavalli@gmail.com>
2014-05-16 15:51:40 +0200

    riformattato

--- test/map.js
+++ test/map.js
@@ -1,23 +1,12 @@
 var map = L.map('map');
 var popup = L.popup();
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent(e.latlng.toString())
-        .openOn(map);
-}
-
 // add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 map.locate({setView: true, maxZoom: 16});
-
-
-map.on('click', onMapClick);
-
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
@@ -29,3 +18,11 @@ function onLocationFound(e) {
 }
 
 map.on('locationfound', onLocationFound);
+
+//function onMapClick(e) {
+//    popup
+//        .setLatLng(e.latlng)
+//        .setContent(e.latlng.toString())
+//        .openOn(map);
+//}
+//map.on('click', onMapClick);

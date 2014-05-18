f0a41a5faf50473f3802eec260382d1f55f084d4
Enrico Cavalli <enrico.cavalli@gmail.com>
2014-05-18 16:20:03 +0200

    stampa delle info

--- test/script.js
+++ test/script.js
@@ -6,11 +6,51 @@ var osm = new L.TileLayer(osmUrl, {
         subdomains: subDomains
 });
 
+
+var info = L.control();
+info.added = false;
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+ this._div.innerHTML = '<h4>Info</h4>' + 
+ (props ? 'Center: '+props.center+'<br>'+'Box: '+props.box : '');
+
+}
+
 var map = new L.Map('map', {
     layers: [osm],
     center: [45, 9],
     zoom: 16,
 	zoomControl: true
+});
+
+function coordToString(c,p) {
+    p = typeof p !== 'undefined' ? p : 3;
+    return c.lat.toFixed(p)+', '+c.lng.toFixed(p)
+}
+
+function displayBoundInformation() {
+ var bounds=map.getBounds();
+    var minll=bounds.getSouthWest();
+    var maxll=bounds.getNorthEast();
+    var center=map.getCenter();
+    var o = Object;
+    o.box= coordToString(minll)+' '+coordToString(maxll);  
+    o.center = coordToString(center);
+    info.update(o);
+
+}
+map.on('locationfound', function(e) { 
+  if ( ! info.added ) {
+    info.addTo(map);
+    info.added=true;
+    }
+    displayBoundInformation();
+    map.on('move', displayBoundInformation);
 });
 
 // add location control to global name space for testing only

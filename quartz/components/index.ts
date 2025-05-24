a92d20381891f5274d6a4a77a9e1171ed52a9ca3
Enrico Cavalli <enrico.cavalli@gmail.com>
2025-05-24 22:07:07 +0200

    Quartz sync: May 24, 2025, 10:07 PM

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -22,6 +22,8 @@ import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import LightBox from "./LightBox"
+import Flex from "./Flex"
+import ConditionalRender from "./ConditionalRender"
 
 export {
   ArticleTitle,
@@ -48,4 +50,6 @@ export {
   Breadcrumbs,
   Comments,
   LightBox,
+  ConditionalRender,
+  Flex,
 }
